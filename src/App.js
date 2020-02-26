773e57be7d566a92c9634c46b63b64145d6ea9b1
Vy Vo <vyvo2510@uw.edu>
2020-02-26 10:52:06 -0800

    second test

--- src/App.js
+++ src/App.js
@@ -1,53 +1,89 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, {Component} from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
+// // function App() {
+// //   return (
+// //     <div className="App">
      
-//     </div>
-//   );
+// //     </div>
+// //   );
+// // }
+
+// class App extends Component{
+//   constructor(props){
+//     super (props);
+//     this.state={
+//       articles:[]
+//     }
+//   }
+//   componentDidMount(){
+//     fetch('http://newsapi.org/v2/everything?domains=wsj.com&apiKey=fc731e84c3eb443698e2d23138af027e')
+//     .then ((response) => {
+//       return response.json();
+//     }
+//     )
+//     .then ((myJson) => {
+//       this.setState({
+//         articles: myJson.articles
+//       });
+//     });
+//   }
+//   render(){
+//     return (
+//       <div className='App'>
+//         {this.state.articles.map((item, index)=> {
+//           return(
+//             <div>
+//               <h2 style ={{textAlign:'left'}}> {item.title} </h2>
+//               <b> {item.author}</b>
+//               <img src = {item.urlToImage} style ={{width: '50vw'}}/>
+//               <a href ='item.url'> Read More </a>
+//               <p>
+//                 {item.content}
+//               </p>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     )
+//   }
 // }
 
-class App extends Component{
-  constructor(props){
-    super (props);
-    this.state={
-      articles:[]
-    }
-  }
-  componentDidMount(){
-    fetch('http://newsapi.org/v2/everything?domains=wsj.com&apiKey=fc731e84c3eb443698e2d23138af027e')
-    .then ((response) => {
-      return response.json();
-    }
-    )
-    .then ((myJson) => {
-      this.setState({
-        articles: myJson.articles
-      });
-    });
-  }
-  render(){
+// export default App;
+
+import React, { Component } from 'react';
+import './App.css';
+import Search from './Search.js';
+import './Search.css';
+import Footer from './Footer.js';
+import './Footer.css';
+import logo from './images/react.svg';
+import news from './images/news.png';
+
+class App extends Component {
+  render() {
     return (
-      <div className='App'>
-        {this.state.articles.map((item, index)=> {
-          return(
-            <div>
-              <h2 style ={{textAlign:'left'}}> {item.title} </h2>
-              <b> {item.author}</b>
-              <img src = {item.urlToImage} style ={{width: '50vw'}}/>
-              <a href ='item.url'> Read More </a>
-              <p>
-                {item.content}
-              </p>
-            </div>
-          );
-        })}
+      <div className="app">
+        <div className="appHeader">
+          <img className="appLogo" src={logo} alt="React Logo" />
+
+          <div className="title">
+            <h1>&lt;HCDE 438 Final Project&gt;</h1>
+            <h4>
+              Powered by
+              <a href="https://newsapi.org/"> News API </a>🔥{' '}
+            </h4>
+          </div>
+
+          <img className="scriptLogo" src={news} alt="Javascript Logo" />
+        </div>
+        <Search default="bbc-news" />
+        <Footer />
       </div>
-    )
+    );
   }
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,95 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+// // const fetch = require ('node-fetch')
+// // const key ='fc731e84c3eb443698e2d23138af027e'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.css';
 
+/*DOM Render*/
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/*Smooth Scrolling Snippet - NO DEPENDENCIES*/
+// Add the class smoothScroll to the links where you want to apply smoothScroll
+// or it will messed up all the links
+var links = document.querySelectorAll('.smoothScroll');
+const transitionSpeed = 10; //transition intervals (ms)
+const initialSpeed = 20; //initial speed (px)
+const acceleration = 0.15; //transition acceleration (px)
+const offset = 10; //offset so the element is not totally in the border (px)
+//const = HTMLMenuElement.height;
+
+links.forEach(link =>
+  link.addEventListener('click', function(e) {
+    //Prevent Default Behaviour
+    e.preventDefault();
+    e.stopPropagation();
+
+    //declare the objective element
+    var goingTo;
+    var hrefID = link.getAttribute('href');
+
+    //In case the # is entered
+    if (hrefID === '#') {
+      goingTo = document.body;
+    } else {
+      goingTo = document.querySelector(hrefID);
+    }
+
+    // var distance = goingTo.offsetTop - link.offsetTop;
+
+    var totalTraveled = 0;
+
+    if (link.offsetTop > goingTo.offsetTop) {
+      //IF GOING UP
+      let speed = -initialSpeed;
+      let distance = goingTo.offsetTop - window.scrollY + offset;
+      console.log('Going up by ' + distance + 'px');
+
+      //Start intervals movement
+      let interval = setInterval(function() {
+        window.scrollBy(0, speed);
+        totalTraveled += speed;
+        speed = Math.round(speed - acceleration);
+
+        //In case it goes over it will make a small jump to fix it
+        if (totalTraveled <= distance) {
+          window.scrollBy(0, totalTraveled - distance);
+          console.log('Fixing distance by ' + (totalTraveled - distance));
+          clearInterval(interval);
+        }
+      }, transitionSpeed);
+    } else {
+      //IF GOING DOWN
+      let distance = goingTo.offsetTop - window.scrollY - offset;
+      let speed = initialSpeed;
+      console.log('Going down by ' + distance + 'px');
+
+      //Start intervals movement
+      let interval = setInterval(function() {
+        window.scrollBy(0, speed);
+        totalTraveled += speed;
+        speed = Math.round(speed + acceleration);
+
+        //In case it goes over it will make a small jump to fix it
+        if (totalTraveled >= distance) {
+          window.scrollBy(0, -(totalTraveled - distance));
+          clearInterval(interval);
+        }
+      }, transitionSpeed);
+    }
+  })
+);
