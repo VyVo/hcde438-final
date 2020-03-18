4182fa1daf56700162a110278f32f40bae0c83ff
Vy Vo <vyvo2510@uw.edu>
2020-03-18 12:08:48 -0700

    final version

---
+++ build/precache-manifest.efe5db75b5c9a7b55e6b848eaa20134d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7d12983f7648b1fbf36285ea47c246",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26916db8434993d25462",
+    "url": "/static/css/main.badcda59.chunk.css"
+  },
+  {
+    "revision": "1c859a1165e3faa89195",
+    "url": "/static/js/2.e34c7ac6.chunk.js"
+  },
+  {
+    "revision": "d2f5423b62d53b191f17197aeaf59a01",
+    "url": "/static/js/2.e34c7ac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26916db8434993d25462",
+    "url": "/static/js/main.67bc0905.chunk.js"
+  },
+  {
+    "revision": "0dd2e7423b0b21cef081",
+    "url": "/static/js/runtime-main.6b87e018.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
